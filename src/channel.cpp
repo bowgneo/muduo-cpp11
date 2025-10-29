bb352c41aac57df2acceec1495f3f9c375ffb4fa
“bowgneo” <bugmaker@seu.edu.cn>
2025-10-29 23:45:56 +0800

    epoll poller

--- src/channel.cpp
+++ src/channel.cpp
@@ -1,11 +1,14 @@
 #include <sys/epoll.h>
+
 #include "logger.h"
 #include "channel.h"
+#include "event_loop.h"
 
 const int Channel::kNoneEvent = 0;
-const int Channel::kReadEvent = EPOLLIN | EPOLLPRI;
+const int Channel::kReadEvent = EPOLLIN;
 const int Channel::kWriteEvent = EPOLLOUT;
 
+// EventLoop: ChannelList Poller
 Channel::Channel(EventLoop *loop, int fd)
     : loop_(loop), fd_(fd), events_(0), revents_(0), index_(-1), tied_(false)
 {
@@ -15,22 +18,30 @@ Channel::~Channel()
 {
 }
 
+// channel的tie方法什么时候调用？一个TcpConnection新连接创建的时候 TcpConnection => Channel 
 void Channel::Tie(const std::shared_ptr<void> &obj)
 {
     tie_ = obj;
     tied_ = true;
 }
 
+/**
+ * 当改变channel所表示fd的events事件后，update负责在poller里面更改fd相应的事件epoll_ctl
+ * EventLoop => ChannelList   Poller
+ */ 
 void Channel::Update()
 {
+    // 通过channel所属的EventLoop，调用poller的相应方法，注册fd的events事件
     loop_->UpdateChannel(this);
 }
 
+// 在channel所属的EventLoop中， 把当前的channel删除掉
 void Channel::Remove()
 {
     loop_->RemoveChannel(this);
 }
 
+// fd得到poller通知以后，处理事件的
 void Channel::HandleEvent(TimeStamp receiveTime)
 {
     if (tied_)
@@ -47,7 +58,8 @@ void Channel::HandleEvent(TimeStamp receiveTime)
     }
 }
 
-void Channel::handleEventWithGuard(TimeStamp receiveTime)
+// 根据poller通知的channel发生的具体事件， 由channel负责调用具体的回调操作
+void Channel::HandleEventWithGuard(TimeStamp receiveTime)
 {
     LOG_INFO("channel handleEvent revents:%d\n", revents_);
 
