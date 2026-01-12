d23d7f63951e500e02bdf762534a6b420f542034
bowgneo <gavin_gq_bao@163.com>
2026-01-12 23:48:33 +0800

    获取和保存线程id

--- include/muduo_current_thread.h
+++ include/muduo_current_thread.h
@@ -3,16 +3,14 @@
 #include <unistd.h>
 #include <sys/syscall.h>
 
-namespace CurrentThread
-{
+namespace CurrentThread {
+    // thread loacl 变量，保存线程 id
     extern __thread int t_cachedTid;
 
     void cacheTid();
 
-    inline int tid()
-    {
-        if (__builtin_expect(t_cachedTid == 0, 0))
-        {
+    inline int tid() {
+        if (__builtin_expect(t_cachedTid == 0, 0)) {
             cacheTid();
         }
         return t_cachedTid;
